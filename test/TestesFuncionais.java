5b971d215ad0f921c478dc4019606a1a27ae77f0
Wilson Shirado <wilson.shirado@animaeducacao.com.br>
2021-08-23 16:37:39 -0300

    adição de teste de login

--- test/TestesFuncionais.java
+++ test/TestesFuncionais.java
@@ -1,5 +1,12 @@
+import java.awt.AWTException;
+import java.awt.Robot;
+import java.awt.event.InputEvent;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import junit.framework.Assert;
 import org.junit.Test;
 import static org.junit.Assert.*;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
@@ -11,7 +18,13 @@ public class TestesFuncionais {
                 "deps/drivers/chromedriver_linux64/chromedriver");
         
         WebDriver browser = new ChromeDriver();
-        browser.navigate().to("http://www.google.com");
+        browser.navigate().to("http://ec2-54-89-211-20.compute-1.amazonaws.com/index");
         
+        browser.findElement(By.id("username")).sendKeys("Hissa");
+        browser.findElement(By.id("password")).sendKeys("senha123");  
+        browser.findElement(By.id("submit")).submit();
+        
+        Assert.assertEquals("Hi, Hissa!", browser.findElement(By.tagName("h1")).getText());
+        browser.quit();
     }
 }
